924af500df7e298e27984bdf3bc1bf5e6b76d2de
enzoperce35 <enzoperce35@gmail.com>
2024-03-12 06:55:21 +0800

    installed and initialized react-facebook dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+"use client";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import { FacebookProvider, CustomChat } from 'react-facebook';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <FacebookProvider appId="910534807474102" chatSupport>
+        <CustomChat pageId="228502743687333" minimized={true} />
+      </FacebookProvider>
     </>
   )
 }
